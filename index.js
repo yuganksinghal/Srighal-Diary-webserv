774d8c42dc437677dbf3e0bd9ec4a870a2f338be
Yugank Singhal <s.yugank@gmail.com>
2015-11-21 20:34:13 -0500

    add post and sql

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var pg = require ('pg');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var db = process.env.DATABASE_URL || 'postgres://vdmhzgburvczes:4a6UvEgPzqmLNlwAIYpG8NedO9@ec2-54-83-199-54.compute-1.amazonaws.com:5432/de8llitlsbuufv?ssl=true'
+
+pg.connect(db, function(err, client){
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client.query('DROP TABLE DIARY');
+    client
+    .query('CREATE TABLE DIARY(ID SERIAl PRIMARY KEY NOT NULL, TITLE CHAR(100) NOT NULL, DIARY_CONTENT TEXT);');
+});
+
 app.get('/', function(req,res){
+    res.send(200);
+});
+
+app.post('/submitentry', function(req,res){
+    console.log("POST");
+    console.log(req.body);
     res.send(200);
 });
 
